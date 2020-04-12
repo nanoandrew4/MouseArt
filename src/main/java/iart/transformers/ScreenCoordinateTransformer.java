7fbbe6ea9bf2064c0ab5d628f1b2f6915d9f2504
nanoandrew4 <nanoandrew4@gmail.com>
2020-04-12 18:13:52 +0200

    Implemented area sharing if two screens would attempt to paint on the same unreachable area, so that each screen takes half of the space for itself, instead of all of it

--- src/main/java/iart/transformers/ScreenCoordinateTransformer.java
+++ src/main/java/iart/transformers/ScreenCoordinateTransformer.java
@@ -19,8 +19,7 @@ public class ScreenCoordinateTransformer {
 	private ScreenCoordinateTransformer() {
 		screens = Screen.generateScreens();
 
-		List<Pair<Screen, ScreenSides>> screenSidesWithoutHorizontalNeighbours = findScreenSidesWithoutHorizontalNeighbours();
-		calculateTransformedBounds(screenSidesWithoutHorizontalNeighbours);
+		calculateTransformedBounds(findScreenSidesWithoutNeighbours());
 	}
 
 	public static void createNewInstance() {
@@ -33,7 +32,11 @@ public class ScreenCoordinateTransformer {
 		return instance;
 	}
 
-	private static List<Pair<Screen, ScreenSides>> findScreenSidesWithoutHorizontalNeighbours() {
+	public void transformPoint(Point p) {
+		screens.stream().filter(screen -> screen.getRealBounds().contains(p.x, p.y)).findFirst().ifPresent(screen -> screen.getTransformer().transform(p));
+	}
+
+	private List<Pair<Screen, ScreenSides>> findScreenSidesWithoutNeighbours() {
 		List<Pair<Screen, ScreenSides>> sidesWithoutDirectNeighbour = new LinkedList<>();
 		screens.forEach(screen -> {
 			if (screen.getRealBounds().getMaxX() < GlobalVariables.screenWidth) {
@@ -51,27 +54,23 @@ public class ScreenCoordinateTransformer {
 		return sidesWithoutDirectNeighbour;
 	}
 
-	public void transformPoint(Point p) {
-		screens.stream().filter(screen -> screen.getRealBounds().contains(p.x, p.y)).findFirst().ifPresent(screen -> screen.getTransformer().transform(p));
-	}
-
 	private void calculateTransformedBounds(List<Pair<Screen, ScreenSides>> screenSidesWithoutNeighbours) {
 		screenSidesWithoutNeighbours.forEach(screenSide -> {
 			if (screenSide.getValue() == ScreenSides.LEFT)
-				calculateTransformedBoundsLeft(screenSide);
+				calculateTransformedBoundsLeftSide(screenSide);
 			else if (screenSide.getValue() == ScreenSides.RIGHT)
-				calculateTransformedBoundsRight(screenSide);
+				calculateTransformedBoundsRightSide(screenSide);
 		});
 	}
 
-	private void calculateTransformedBoundsRight(Pair<Screen, ScreenSides> screenSide) {
+	private void calculateTransformedBoundsRightSide(Pair<Screen, ScreenSides> screenSide) {
 		Screen currentScreen = screenSide.getKey();
 		Predicate<Rectangle2D> hasNeighbourToRight = currentScreen::hasNeighbourToRight;
 		Comparator<Rectangle2D> minXComparator = Comparator.comparingDouble(Rectangle2D::getMinX);
 		Rectangle2D nearestScreenBounds = screens.stream().map(Screen::getRealBounds).filter(hasNeighbourToRight).min(minXComparator).orElse(null);
 
 		Rectangle2D prevTransformedBounds = currentScreen.getTransformedBounds();
-		currentScreen.setTransformedBoundsMaxX(nearestScreenBounds == null ? GlobalVariables.screenWidth : nearestScreenBounds.getMinX());
+		currentScreen.setTransformedBoundsMaxX(calculateTransformedBoundsMaxX(currentScreen, nearestScreenBounds));
 		Rectangle2D newTransformedBounds = currentScreen.getTransformedBounds();
 		boolean newBoundsIntersect = screens.stream().filter(screen -> !screen.equals(currentScreen)).map(Screen::getTransformedBounds).anyMatch(newTransformedBounds::intersects);
 		if (newBoundsIntersect) {
@@ -80,19 +79,44 @@ public class ScreenCoordinateTransformer {
 		}
 	}
 
-	private void calculateTransformedBoundsLeft(Pair<Screen, ScreenSides> screenSide) {
+	private double calculateTransformedBoundsMaxX(Screen screen, Rectangle2D nearestScreenBounds) {
+		if (nearestScreenBounds == null)
+			return GlobalVariables.screenWidth;
+
+		Rectangle2D screenBounds = screen.getRealBounds();
+
+		boolean shareUnreachableArea = canShareUnreachableHorizontalArea(screenBounds, nearestScreenBounds);
+		return shareUnreachableArea ? (screenBounds.getMaxX() + (nearestScreenBounds.getMinX() - screenBounds.getMaxX()) / 2) : nearestScreenBounds.getMinX();
+	}
+
+	private void calculateTransformedBoundsLeftSide(Pair<Screen, ScreenSides> screenSide) {
 		Screen currentScreen = screenSide.getKey();
 		Predicate<Rectangle2D> hasNeighbourToLeft = currentScreen::hasNeighbourToLeft;
 		Comparator<Rectangle2D> maxXComparator = Comparator.comparingDouble(Rectangle2D::getMaxX);
 		Rectangle2D nearestScreenBounds = screens.stream().map(Screen::getRealBounds).filter(hasNeighbourToLeft).min(maxXComparator).orElse(null);
 
 		Rectangle2D prevTransformedBounds = currentScreen.getTransformedBounds();
-		currentScreen.setTransformedBoundsMinX(nearestScreenBounds == null ? 0 : nearestScreenBounds.getMaxX());
+		currentScreen.setTransformedBoundsMinX(calculateTransformedBoundsMinX(currentScreen, nearestScreenBounds));
 		Rectangle2D newTransformedBounds = currentScreen.getTransformedBounds();
 		boolean newBoundsIntersect = screens.stream().filter(screen -> !screen.equals(currentScreen)).map(Screen::getTransformedBounds).anyMatch(newTransformedBounds::intersects);
 		if (newBoundsIntersect) {
 			System.err.println("Intersection occurred while determining left side transformed bounds for screen: " + currentScreen);
 			currentScreen.setTransformedBounds(prevTransformedBounds);
 		}
+	}
+
+	private double calculateTransformedBoundsMinX(Screen screen, Rectangle2D nearestScreenBounds) {
+		if (nearestScreenBounds == null)
+			return 0;
+
+		Rectangle2D screenBounds = screen.getRealBounds();
+
+		boolean shareUnreachableArea = canShareUnreachableHorizontalArea(screenBounds, nearestScreenBounds);
+		return shareUnreachableArea ? (screenBounds.getMinX() - (screenBounds.getMinX() - nearestScreenBounds.getMaxX()) / 2) : nearestScreenBounds.getMaxX();
+	}
+
+	private boolean canShareUnreachableHorizontalArea(Rectangle2D bounds1, Rectangle2D bounds2) {
+		return (bounds1.getMinY() >= bounds2.getMinY() && bounds1.getMaxY() <= bounds2.getMaxY()) ||
+			   (bounds1.getMinY() <= bounds2.getMinY() && bounds1.getMaxY() >= bounds2.getMaxY());
 	}
 }
